809c105a083519f9ccde332525319fa51eea0ead
ahdbasan <ahdbasan2@example.com>
2024-12-14 07:32:17 +0300

    another answer

--- 2_predictive_stepping/examples/00_pausing_on_errors.py
+++ 2_predictive_stepping/examples/00_pausing_on_errors.py
@@ -11,10 +11,20 @@ You can change this setting in the breakpoints pane down left.
 
 a = 1
 
-b = "2"
+b = 2     #"2" str! = int  *type_error
 
 # the error message will appear below this line
 #   and the values in memory can help you understand what went wrong
 c = a + b
 
+print(c) 
+
+
+
+#RUNTIME ERROR
+#OR SOLVE IT LIKE THAT!!
+
+a=1
+b="2"
+c = a + int (b)
 print(c)
